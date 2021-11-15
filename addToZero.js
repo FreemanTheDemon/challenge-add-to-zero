3900862a8e6fb5dd250ba2f10f9b2ec5386c7a05
Freeman Bird <freemanbird@gmail.com>
2021-11-15 09:41:53 -0700

    Challenge complete

--- addToZero.js
+++ addToZero.js
@@ -2,3 +2,22 @@
 let array = [28, 43, -12, 30, 4, 0, 12]
 
 // Write your solution below:
+
+function addToZero(arr) {
+    var zeroable = false;
+    for (var i = 0; i < arr.length; i++) {
+        var currentNum = arr[i];
+        for (var j = 0; j < arr.length; j++) {
+            var innerNum = arr[j];
+            if (i === j) {
+                continue;
+            } else if (currentNum + innerNum === 0) {
+                zeroable = true;
+            }
+        }
+    }
+
+    console.log(zeroable);
+}
+
+addToZero(array);
